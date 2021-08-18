4c9645e5215d8d976569d5e461f8d7051d899266
junaiyet <junaiyetmia470@gmail.com>
2021-08-18 12:26:23 +0600

    test

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ $('.counter').counterUp({
     delay: 10,
     time: 1000
 });
+
+
+// ////////////////////
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+      $(".navigation").addClass("fixd-manu")
+    } else {
+      $(".navigation").removeClass("fixd-manu")
+    }
+  
+  })
+  
